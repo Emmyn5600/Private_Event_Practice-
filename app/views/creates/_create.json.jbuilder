ae8a9fd46d9b1165a74d836851c4441716e27d22
emmy <emmy66418@gmail.com>
2021-04-30 17:33:45 +0200

    Add scaffold and change the root

---
+++ app/views/creates/_create.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! create, :id, :Event, :title, :description, :location, :date, :created_at, :updated_at
+json.url create_url(create, format: :json)
---
+++ db/migrate/20210430153019_create_creates.rb
@@ -0,0 +1,13 @@
+class CreateCreates < ActiveRecord::Migration[6.1]
+  def change
+    create_table :creates do |t|
+      t.string :Event
+      t.string :title
+      t.text :description
+      t.string :location
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+end
